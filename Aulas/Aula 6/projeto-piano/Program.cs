5df2d942b070eec93f53f414fd5c9e49cf771ca6
PatrickAndriol1 <patrickandrioli@hotmail.com>
2024-03-02 12:14:39 -0300

    piano

---
+++ Aulas/Aula 6/projeto-piano/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program 
+{
+    static void Main(string[] args)
+    {
+        const int CONTADOR = 7;
+        int oitava = 1;
+        int nota = 1;
+
+        while (nota <= CONTADOR)
+        {
+            Piano.ReproduzTecla(oitava,nota);
+            nota++;
+        }
+    }
+}
