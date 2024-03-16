a4110fd69f38299c5cba451236016509a434d42b
PatrickAndriol1 <patrickandrioli@hotmail.com>
2024-03-16 12:29:18 -0300

    jogos

---
+++ Jogos/Ano novo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        for(int segundos = 3; segundos > 0; segundos--)
+        {
+            System.Console.WriteLine(segundos);
+            Thread.Sleep(1000);
+        }
+        System.Console.WriteLine("Feliz ano novo!!!");
+    }
+}
