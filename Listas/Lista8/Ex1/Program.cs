564099f67abc734a5e522e30b2dbba19e84b8ce7
PatrickAndriol1 <patrickandrioli@hotmail.com>
2024-04-11 18:08:52 -0300

    lista8

---
+++ Listas/Lista8/Ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+struct Pizza
+{
+    public int Id;
+    public String Sabor;
+    public double Preco;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza pizza1;
+        pizza1.Id = Convert.ToInt32(Console.ReadLine());
+        pizza1.Sabor = Console.ReadLine();
+        pizza1.Preco = Convert.ToDouble(Console.ReadLine());
+
+        System.Console.WriteLine(pizza1.Id);
+        System.Console.WriteLine(pizza1.Sabor);
+        System.Console.WriteLine(pizza1.Preco);
+    }
+}
---
+++ Listas/Lista8/Ex2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+struct Produto
+{
+    public int Id;
+    public String nome;
+    public double preco;
+    public bool disponivelEmEstoque;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int TAMANHO_MATRIZ = 4;
+        Produto[] produtos = new Produto[TAMANHO_MATRIZ];
+        PreencherMatriz(produtos);
+        MostrarProdutosDisponiveis(produtos);
+    }
+
+    static void PreencherMatriz(Produto[] produtos)
+    {
+        for(int i = 0; i < produtos.Length; i++)
+        {
+            produtos[i].Id = Convert.ToInt32(Console.ReadLine());
+            produtos[i].nome = Console.ReadLine();
+            produtos[i].preco = Convert.ToDouble(Console.ReadLine());
+            produtos[i].disponivelEmEstoque = Convert.ToBoolean(Console.ReadLine());
+        }
+    }
+
+    static void MostrarProdutosDisponiveis(Produto[] produtos)
+    {
+        for(int i = 0; i < produtos.Length; i++)
+        {
+            if(produtos[i].disponivelEmEstoque == true)
+            {
+                System.Console.WriteLine(produtos[i].nome);
+                System.Console.WriteLine(produtos[i].preco);
+            }
+        }
+    }
+}
